4e9ef11395d24e0f153186b58823436fbb8ae39f
meishaaaaa <meishaaaaa@163.com>
2020-02-19 22:19:55 +0800

    change1

--- map/src/main/java/Application.java
+++ map/src/main/java/Application.java
@@ -1,6 +1,39 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
 public class Application {
 
-  public static void main(String[] args) {
-  }
+    public static void main(String[] args) {
+        Student stu1 = new Student("Linda", 18);
+        Student stu2 = new Student("Cindy", 19);
+        Student stu3 = new Student("Mary", 19);
+        Student stu4 = new Student("Ben", 19);
+        Student stu5 = new Student("Solider", 20);
+
+        Map<String, Integer> stuList = new HashMap<>();
+        stuList.put(stu1.getName(), stu1.getAge());
+        stuList.put(stu2.getName(), stu2.getAge());
+        stuList.put(stu3.getName(), stu3.getAge());
+
+        Teacher tea = new Teacher();
+        tea.setName("Bob");
+        tea.setStu(stuList);
+
+        tea.addStudent(stu4);
+        tea.addStudent(stu5);
+
+        Set<Map.Entry<String, Integer>> set = tea.getStu().entrySet();
+        Iterator<Map.Entry<String, Integer>> it = set.iterator();
+
+        while (it.hasNext()) {
+            Map.Entry<String, Integer> entry = it.next();
+            String student = entry.getKey();
+            Integer age = entry.getValue();
+            System.out.println(student + "," + age);
+        }
+
+    }
 
 }
--- map/src/main/java/Teacher.java
+++ map/src/main/java/Teacher.java
@@ -1,3 +1,46 @@
-public class Teacher {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
+public class Teacher {
+    private String name;
+    private Map<String, Integer> stu = null;
+
+    public Teacher() {
+        stu = new HashMap<>();
+    }
+
+    public Teacher(String name, Map<String, Integer> stu) {
+        this.name = name;
+        this.stu = stu;
+    }
+
+    public void addStudent(Student student) {
+        Integer age = student.getAge();
+        String name = student.getName();
+        stu.put(name, age);
+    }
+
+    public void sortStudent(Map<String, Integer> stu) {
+        Map<Integer, List<Entry<String, Integer>>> result = stu.entrySet().stream().collect(Collectors.groupingBy(c -> c.getValue()));
+        System.out.println(result);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Map<String, Integer> getStu() {
+        return stu;
+    }
+
+    public void setStu(Map<String, Integer> stu) {
+        this.stu = stu;
+    }
 }
